97f61c7c7b57bc9acb3420e2c205db72536bac5b
fachher.syed <fachher@googlemail.com>
2019-01-24 22:57:16 +0100

    Added spi example

---
+++ ch07-spi-leds/src/main.c
@@ -0,0 +1,37 @@
+#include "lpc17xx.h"
+#include "spi.h"
+
+volatile uint32_t msTicks;                            /* counts 1ms timeTicks */
+/*----------------------------------------------------------------------------
+  SysTick_Handler
+ *----------------------------------------------------------------------------*/
+void SysTick_Handler(void) {
+  msTicks++;                        /* increment counter necessary in Delay() */
+}
+
+/*------------------------------------------------------------------------------
+  delays number of tick Systicks (happens every 1 ms)
+ *------------------------------------------------------------------------------*/
+__INLINE static void Delay (uint32_t dlyTicks) {
+  uint32_t curTicks;
+
+  curTicks = msTicks;
+  while ((msTicks - curTicks) < dlyTicks);
+}
+
+/*----------------------------------------------------------------------------
+  MAIN function
+ *----------------------------------------------------------------------------*/
+int main (void) {
+
+  if (SysTick_Config(SystemCoreClock / 1000)) { /* Setup SysTick Timer for 1 msec interrupts  */
+    while (1);                                  /* Capture error */
+  }
+
+  while(1)
+  {
+
+  }
+
+}
+
